6c60aa159e216fd834cbd26a41fb4149d7271ba3
Ben Smith <benjsmi@us.ibm.com>
2014-04-20 20:16:32 -0400

    Using newer twitter APIs now

--- news.php
+++ news.php
@@ -4,35 +4,8 @@ include "functions.php";
 
 printHeader();
 ?>
-<div id="twitterfeed" style="">
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: 'profile',
-  rpp: 4,
-  interval: 30000,
-  width: 600,
-  height: 300,
-  theme: {
-    shell: {
-      background: '#333333',
-      color: '#999999'
-    },
-    tweets: {
-      background: '#000000',
-      color: '#6b6b6b',
-      links: '#ffffff'
-    }
-  },
-  features: {
-    scrollbar: true,
-    loop: false,
-    live: false,
-    behavior: 'all'
-  }
-}).render().setUser('BINKUpdates').start();
-</script> 
-</div>	
+<a class="twitter-timeline" href="https://twitter.com/BINKUpdates" data-widget-id="458034947404492800">Tweets by @BINKUpdates</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <?php
 printFooter();
 
