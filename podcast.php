b58f74b60beaad961cf1f2e579f76c35dde0995f
Ben Smith <benjsmi@us.ibm.com>
2014-04-20 20:06:54 -0400

    The podcast how-to.  That's useful stuff, Jim.

---
+++ podcast.php
@@ -0,0 +1,40 @@
+<?php
+
+include "functions.php";
+
+printHeader();
+?>
+
+<div class="item">
+<h2>Accessing the BINK Podcast from iPhone</h2>
+You can download and listen to the tunes from BINK! in a synchronized
+podcast, available from iTunes, or directly from your phone.  To access
+the podcast from iTunes, check [ <a href="https://itunes.apple.com/us/podcast/bink!/id629595170">this link</a> ].
+<p /><br />
+To access the podcast from your phone, get your phone 
+handy and follow these steps.<p /><br />
+<h1>Step 1. Open the Podcasts App on your iPhone</h1>
+<img src="/img/podcast/photo2.png" width=200 />
+
+<h1>Step 2. In the Podcasts app, tap "Store" to switch the view to iTunes store</h1>
+<img src="/img/podcast/photo6.png" width=200 />
+
+<h1>Step 3. In the iTunes store, tap "Search" and enter 'bink'</h1>
+<img src="/img/podcast/photo5.png" width=200 />
+
+<h1>Step 4. In the information listing for BINK!, tap "subscribe"</h1>
+<img src="/img/podcast/photo3.png" width=200 />
+
+<h1>Step 5. You can now browse the tunes in the downloaded podcast</h1>
+<img src="/img/podcast/photo1.png" width=200 />
+
+<h1>Step 6. And download individual songs from collections</h1>
+<img src="/img/podcast/photo4.png" width=200 />
+
+
+
+</div>
+<?php
+printFooter();
+
+?>
