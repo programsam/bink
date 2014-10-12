39f0803b238cd1247310d09b557489ec27d05335
programsam <programsam@gmail.com>
2014-10-12 16:14:49 -0400

    Customize block size.

--- functions.php
+++ functions.php
@@ -1386,7 +1386,7 @@ if ($maps)
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- binkmusic -->
 <ins class="adsbygoogle"
-     style="display:inline-block;width:120px;height:600px"
+     style="display:inline-block;width:820px;height:200px"
      data-ad-client="ca-pub-1453391221011492"
      data-ad-slot="7514258764"></ins>
 <script>
