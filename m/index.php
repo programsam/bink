f87ed892afc073f7b140456a174391ee16b9d509
Ben Smith <programsam@gmail.com>
2017-06-03 10:29:45 -0400

    Another isset

--- m/index.php
+++ m/index.php
@@ -3,7 +3,7 @@
 include "functions.php";
 
 printHeader();
-if ($_GET['dir'] == 1)
+if (isset($_GET['dir']) && $_GET['dir'] == 1)
 		echo getJams("select * from jams where private=0 order by id desc limit 0,10");
 	else
 		echo getJams("select * from jams where private=0 order by date desc limit 0,10");
