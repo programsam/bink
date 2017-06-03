3e78ad16e44316105b6c10dd9a6c040667d76ef4
Ben Smith <programsam@gmail.com>
2017-06-03 10:37:37 -0400

    More issets.

--- m/musician.php
+++ m/musician.php
@@ -2,7 +2,7 @@
 	include "functions.php";			
 	printHeader();
 	echo getMusicianInfo($_GET['query']);
-	if ($_GET['offset'] || $_GET['length'] || $_GET['order'] || $_GET['sort'])
+	if (isset($_GET['offset']) || isset($_GET['length']) || isset($_GET['order']) || isset($_GET['sort']))
 		echo getJamsSearch(1, $_GET['query'], $_GET['offset'], $_GET['length'], $_GET['order'], $_GET['sort'], 2);
 	else
 		echo getJamsSearch(1, $_GET['query']);
