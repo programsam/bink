78aecf50411e878d46fe7b06a44507a373007227
Ben Smith <programsam@gmail.com>
2017-11-26 21:07:45 -0500

    Return to facebook code.

--- functions.php
+++ functions.php
@@ -1224,24 +1224,13 @@ $adminStr = "admin/main.php";
 </head>
 <body onload="initialize();">
 <div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '139182602788074',
-      xfbml      : true,
-      version    : 'v2.11'
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=139182602788074';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 <div class="main">
 	<div class="gfx">
