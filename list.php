47d8c5da547bef0d33abcb2e95a3f4a12a5dd04b
Ben Smith <programsam@gmail.com>
2021-06-07 12:10:00 -0400

    Catch more undefineds.

--- list.php
+++ list.php
@@ -1,9 +1,24 @@
 <?php
-	include "functions.php";			
+	include "functions.php";
 	printHeader();
-	if ($_GET['offset'] || $_GET['length'] || $_GET['order'] || $_GET['sort'])
-		echo getJamsSearch(0, null, $_GET['offset'], $_GET['length'], $_GET['order'], $_GET['sort']);
-	else
+	if ($_GET['offset'] || $_GET['length'] || $_GET['order'] || $_GET['sort']) {
+		$offset = 0;
+		$length = 10;
+		$order="date";
+		$sort="desc";
+		if (isset($_GET['offet']))
+			$offset = $_GET['offset'];
+		if (isset($_GET['length']))
+			$length = $_GET['length'];
+		if (isset($_GET['order']))
+			$order = $_GET['order'];
+		if (isset($_GET['sort']))
+			$sort = $_GET['sort'];
+
+		echo getJamsSearch(0, null, $offset, $length, $order, $sort);
+	}
+	else {
 		echo getJamsSearch(0, null);
+	}
 	printFooter();
 ?>
