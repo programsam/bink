d98450040e4c6ab2d29da0433623c7a0f0c46a42
Ben Smith <programsam@gmail.com>
2021-06-07 12:42:58 -0400

    Get the mobile list, too.

--- m/list.php
+++ m/list.php
@@ -1,9 +1,27 @@
 <?php
-	include "functions.php";			
+	include "functions.php";
 	printHeader();
-	if ($_GET['offset'] || $_GET['length'] || $_GET['order'] || $_GET['sort'])
-		echo getJamsSearch(0, null, $_GET['offset'], $_GET['length'], $_GET['order'], $_GET['sort']);
-	else
+	if (isset($_GET['offset'])
+			|| isset($_GET['length'])
+			|| isset($_GET['order'])
+			|| isset($_GET['sort'])) {
+		$offset = 0;
+		$length = 20;
+		$order="date";
+		$sort="desc";
+		if (isset($_GET['offset']))
+			$offset = $_GET['offset'];
+		if (isset($_GET['length']))
+			$length = $_GET['length'];
+		if (isset($_GET['order']))
+			$order = $_GET['order'];
+		if (isset($_GET['sort']))
+			$sort = $_GET['sort'];
+
+		echo getJamsSearch(0, null, $offset, $length, $order, $sort);
+	}
+	else {
 		echo getJamsSearch(0, null);
+	}
 	printFooter();
 ?>
