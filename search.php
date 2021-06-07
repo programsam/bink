04d53a85ff897b934df1a7afdd040e7b17083986
Ben Smith <programsam@gmail.com>
2021-06-07 12:20:15 -0400

    Fix it for generic search, too.

--- search.php
+++ search.php
@@ -1,9 +1,25 @@
 <?php
-	include "functions.php";			
+	include "functions.php";
 	printHeader();
-	if ($_GET['offset'] || $_GET['length'] || $_GET['order'] || $_GET['sort'])
+	if (isset($_GET['offset'])
+			|| isset($_GET['length'])
+			|| isset($_GET['order'])
+			|| isset($_GET['sort'])) {
+		$offset = 0;
+		$length = 20;
+		$order="date";
+		$sort="desc";
+		if (isset($_GET['offet']))
+			$offset = $_GET['offset'];
+		if (isset($_GET['length']))
+			$length = $_GET['length'];
+		if (isset($_GET['order']))
+			$order = $_GET['order'];
+		if (isset($_GET['sort']))
+			$sort = $_GET['sort'];
 		echo getJamsSearch(2, $_GET['query'], $_GET['offset'], $_GET['length'], $_GET['order'], $_GET['sort'], 2);
-	else
+	} else {
 		echo getJamsSearch(2, $_GET['query']);
+	}
 	printFooter();
 ?>
