83e733fd90dc560c8fc1257a596ef2ae6735320e
Ben Smith <programsam@gmail.com>
2023-07-30 14:54:21 -0400

    Forgot a place where we were using /var/tmp instead of /tmp.

--- viewlog.php
+++ viewlog.php
@@ -1,15 +1,6 @@
 <?php
 
-$x = file_get_contents("/var/tmp/downloadlog");
+$x = file_get_contents("/tmp/downloadlog");
 
 echo $x;
-	
-
-/*
-if ($pos === FALSE)
-	echo $x;
-else
-	echo "The file has finished downloading.";
-	*/
-//echo $x;
 ?>
